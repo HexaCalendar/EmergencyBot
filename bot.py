8dd28c998e71eb4add83213c2ec8c347e08d1a70
SSKATE <kms0219kms@naver.com>
2022-02-17 18:56:33 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -70,7 +70,7 @@ async def on_member_join(member):
           
   # Alts Kicker
   days = datetime.now().replace(tzinfo=None) - member.created_at.replace(tzinfo=None)
-  if days < timedelta(days=30):
+  if days < timedelta(days=40):
     
       file = open('whitelist.json', 'r')
       x = file.read()
@@ -81,7 +81,7 @@ async def on_member_join(member):
       try:
         embed = disnake.Embed(
           title = f"{setup_name} 자동 차단",
-          description = f'Discord 계정이 가입한지 30일이 경과되지 않았어요.\n테러 방지 차원으로 이런 조치를 취하게 되어 양해 부탁드려요.'
+          description = f'Discord 계정이 가입한지 40일이 경과되지 않았어요.\n테러 방지 차원으로 이런 조치를 취하게 되어 양해 부탁드려요.'
         )
         dm = await member.create_dm()
         await dm.send(embed = embed, view = Link())
@@ -138,7 +138,7 @@ async def on_member_join(member):
 async def alts(ctx, day:int=None):
   # Alts Kicker
   if day is None:
-        day = 30
+        day = 40
   for member in ctx.guild.members:
     days = datetime.now().replace(tzinfo=None) - member.created_at.replace(tzinfo=None)
     if days < timedelta(days=day):
@@ -146,7 +146,7 @@ async def alts(ctx, day:int=None):
         try:
           embed = disnake.Embed(
             title = f"{setup_name} 자동 차단",
-            description = f'Discord 계정이 가입한지 30일이 경과되지 않았어요.\n테러 방지 차원으로 이런 조치를 취하게 되어 양해 부탁드려요.'
+            description = f'Discord 계정이 가입한지 40일이 경과되지 않았어요.\n테러 방지 차원으로 이런 조치를 취하게 되어 양해 부탁드려요.'
           )
           dm = await member.create_dm()
           await dm.send(embed = embed, view = Link())
